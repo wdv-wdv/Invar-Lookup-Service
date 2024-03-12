5d0da00bcddac516a62172516c07f90928799faf
Setup boilerplate <setup-boilerplate@example.com>
2024-03-12 19:33:38 +0000

    Initialize boilerplate backend-boilerplate-dotnet

---
+++ app/backend/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "pong";
+        }
+    }
+}
--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   env: {
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://localhost:5000',
   },
   e2e: {
     setupNodeEvents(on, config) {},
